04019217d6893f4533d8f8d2f549d1d4c254f9e5
Alex Bennée <alex.bennee@linaro.org>
2017-10-17 11:18:05 +0100

    vector-benchmark: make harder

--- aarch64/vector-benchmark.c
+++ aarch64/vector-benchmark.c
@@ -7,38 +7,78 @@
 #include <inttypes.h>
 #include <time.h>
 
-#define OPS     (1<<15)
+#define OPS     (1<<22)
 #define ALIGNED_OPS (OPS/(sizeof(uint64_t)/(sizeof(uint8_t))))
+
+/* Generate an array of psuedo-random data to feed into our
+ * vectorisable kernel. The array needs to be nicely aligned to
+ * help the vectoriser
+ */
+static uint8_t * get_data(uint32_t seed)
+{
+    unsigned long i;
+    long int rseed = random();
+    const int total = OPS + 0x100;
+    uint8_t *data = malloc(total * sizeof(uint8_t));
+
+    fprintf(stderr, "%s: with %#" PRIx32 "/%#lx\n", __func__, seed, rseed);
+
+    for (i=0; i < total; i = i + 4)
+    {
+        data[i]   = ((rseed >>  8) ^ (seed >>  0) ^ i) & 0xff;
+        data[i+1] = ((rseed >> 16) ^ (seed >>  8) ^ i) & 0xff;
+        data[i+2] = ((rseed >> 24) ^ (seed >> 16) ^ i) & 0xff;
+        data[i+3] = ((rseed >>  0) ^ (seed >> 24) ^ i) & 0xff;
+    }
+
+    return data;
+}
+
+static inline uint8_t * align_data(uint8_t *data)
+{
+    uintptr_t ptr = (uintptr_t) data;
+    ptr = (ptr + (8 - 1)) & -8;
+    return (uint8_t *) ptr;
+}
+
+static inline int64_t get_clock(void)
+{
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    return ts.tv_sec * 1000000000LL + ts.tv_nsec;
+}
 
 int main(void)
 {
-    uint8_t *inA, *inB, *inC, *out;
-    time_t start, end;
-    int i;
+    uint8_t *add, *sub, *or, *xor, *out;
+    int64_t start, end;
+    unsigned long i, j;
 
-    inA = calloc(ALIGNED_OPS, sizeof(uint64_t));
-    inB = calloc(ALIGNED_OPS, sizeof(uint64_t));
-    inC = calloc(ALIGNED_OPS, sizeof(uint64_t));
-    out = calloc(ALIGNED_OPS, sizeof(uint64_t));
+    add = align_data(get_data(0xABABCDCD));
+    sub = align_data(get_data(0xDEEDBEEF));
+    or  = align_data(get_data(0x10101010));
+    xor = align_data(get_data(0x7e7e7e7e));
+    out = align_data(get_data(0x00000000));
 
-    /* Seed inA/B */
-    for (i = 0; i < OPS; i++)
-    {
-        inA[i] = i % 129;
-        inB[i] = i % 126;
-        inC[i] = inA[i] ^ inB[i];
-    }
-
-    start = time(NULL);
+    start = get_clock();
 
     /* Twiddle bits */
-    for (i = 0; i < OPS; i++)
-    {
-        out[i] = ~(inA[i] | inB[i] & inC[i]);
+    for (j = 0; j < 100; j++) {
+        for (i = 0; i < OPS; i++)
+        {
+            uint8_t value = out[i];
+            value += add[i];
+            value ^= xor[i];
+            value -= sub[i];
+            value |= or[i];
+            value ^= xor[i];
+            out[i] = value;
+        }
     }
 
-    end = time(NULL);
+    end = get_clock();
 
+    fprintf(stderr, "%s: ran %ld x %ld cycles in %ld\n", __func__, i, j, end - start);
 
     return 0;
 }
